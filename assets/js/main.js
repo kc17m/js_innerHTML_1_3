76b98e691703ecd7001b099bcd6418e83f4b22d4
SuperCoder <supercode@AirvonSuperCode.fritz.box>
2022-01-18 15:10:58 +0100

    width, display added

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,6 @@ let img1 = document.getElementById("gallery");
 
 console.log(img1);
 
-img1.innerHTML = '<figure><img src="assets/img/photo-1642421805007-c42ab7bdecba.jpeg" alt="img"><figcaption>Mein Bild</figcaption><img src="assets/img/photo-1642421805007-c42ab7bdecba.jpeg" alt="img"><figcaption>Mein Bild</figcaption><img src="assets/img/photo-1642421805007-c42ab7bdecba.jpeg" alt="img"><figcaption>Mein Bild</figcaption></figure>';
+img1.innerHTML = '<figure style="display:inline-block"><img src="assets/img/photo-1642421805007-c42ab7bdecba.jpeg" width="70%" alt="img"><figcaption>Mein Bild</figcaption></figure> <figure style="display:inline-block"><img src="assets/img/photo-1642421805007-c42ab7bdecba.jpeg" width="70%" alt="img"><figcaption>Mein Bild</figcaption></figure> <figure style="display:inline-block"><img src="assets/img/photo-1642421805007-c42ab7bdecba.jpeg" width="70%" alt="img"><figcaption>Mein Bild</figcaption></figure>';
 
 
