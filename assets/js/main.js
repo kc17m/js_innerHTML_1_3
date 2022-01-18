60ff562cac9d7904d983fc67f25852b721db643b
SuperCoder <supercode@AirvonSuperCode.fritz.box>
2022-01-18 14:42:48 +0100

    2

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,4 +5,4 @@ let img1 = document.getElementById("gallery");
 
 console.log(img1);
 
-img1.innerHTML = '<figure><img src="../assets/img/photo-1642421805007-c42ab7bdecba.jpeg" alt="img"><figcaption>Mein Bild</figcaption><img src="../assets/img/photo-1642421805007-c42ab7bdecba.jpeg" alt="img"><figcaption>Mein Bild</figcaption><img src="../assets/img/photo-1642421805007-c42ab7bdecba.jpeg" alt="img"><figcaption>Mein Bild</figcaption></figure>';
+img1.innerHTML = '<figure><img src="assets/img/photo-1642421805007-c42ab7bdecba.jpeg" alt="img"><figcaption>Mein Bild</figcaption><img src="assets/img/photo-1642421805007-c42ab7bdecba.jpeg" alt="img"><figcaption>Mein Bild</figcaption><img src="assets/img/photo-1642421805007-c42ab7bdecba.jpeg" alt="img"><figcaption>Mein Bild</figcaption></figure>';
