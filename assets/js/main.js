0aa6e41c58e3ac4f659f422b24b2599bb2bc4396
SuperCoder <supercode@AirvonSuperCode.fritz.box>
2022-01-18 14:40:25 +0100

    1

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+// document.getElementById("gallery").innerHTML = '<figure><img src="../assets/img/photo-1642421805007-c42ab7bdecba.jpeg" alt="img"><figcaption>Mein Bild</figcaption><figure><img src="../assets/img/photo-1642421805007-c42ab7bdecba.jpeg" alt="img"><figcaption>Mein Bild</figcaption><figure><img src="../assets/img/photo-1642421805007-c42ab7bdecba.jpeg" alt="img"><figcaption>Mein Bild</figcaption>';
+
+
+let img1 = document.getElementById("gallery");
+
+console.log(img1);
+
+img1.innerHTML = '<figure><img src="../assets/img/photo-1642421805007-c42ab7bdecba.jpeg" alt="img"><figcaption>Mein Bild</figcaption><img src="../assets/img/photo-1642421805007-c42ab7bdecba.jpeg" alt="img"><figcaption>Mein Bild</figcaption><img src="../assets/img/photo-1642421805007-c42ab7bdecba.jpeg" alt="img"><figcaption>Mein Bild</figcaption></figure>';
