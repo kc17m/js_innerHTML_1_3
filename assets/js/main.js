873bcf170cc063bc0138ac5afbc7bb1bac0ef03f
SuperCoder <supercode@AirvonSuperCode.fritz.box>
2022-01-18 14:49:56 +0100

    3

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,5 @@ let img1 = document.getElementById("gallery");
 console.log(img1);
 
 img1.innerHTML = '<figure><img src="assets/img/photo-1642421805007-c42ab7bdecba.jpeg" alt="img"><figcaption>Mein Bild</figcaption><img src="assets/img/photo-1642421805007-c42ab7bdecba.jpeg" alt="img"><figcaption>Mein Bild</figcaption><img src="assets/img/photo-1642421805007-c42ab7bdecba.jpeg" alt="img"><figcaption>Mein Bild</figcaption></figure>';
+
+
