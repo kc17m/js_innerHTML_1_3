01df8b0ba65efe484badbb82c1ffa20d0d396c84
SuperCoder <supercode@AirvonSuperCode.fritz.box>
2022-01-18 15:19:54 +0100

    image caption updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,6 @@ let img1 = document.getElementById("gallery");
 
 console.log(img1);
 
-img1.innerHTML = '<figure style="display:inline-block"><img src="assets/img/photo-1642421805007-c42ab7bdecba.jpeg" width="70%" alt="img"><figcaption>Mein Bild</figcaption></figure> <figure style="display:inline-block"><img src="assets/img/photo-1642421805007-c42ab7bdecba.jpeg" width="70%" alt="img"><figcaption>Mein Bild</figcaption></figure> <figure style="display:inline-block"><img src="assets/img/photo-1642421805007-c42ab7bdecba.jpeg" width="70%" alt="img"><figcaption>Mein Bild</figcaption></figure>';
+img1.innerHTML = '<figure style="display:inline-block"><img src="assets/img/photo-1642421805007-c42ab7bdecba.jpeg" width="70%" alt="img"><figcaption>Mein Bild 01</figcaption></figure> <figure style="display:inline-block"><img src="assets/img/photo-1642421805007-c42ab7bdecba.jpeg" width="70%" alt="img"><figcaption>Mein Bild 02</figcaption></figure> <figure style="display:inline-block"><img src="assets/img/photo-1642421805007-c42ab7bdecba.jpeg" width="70%" alt="img"><figcaption>Mein Bild 03</figcaption></figure>';
 
 
